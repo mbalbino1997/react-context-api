8590d0e214928773521451ba6caede263d7d0167
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-12-11 17:36:20 +0100

    add: context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/home/home.jsx"
 import AboutUs from './pages/aboutus/AboutUs.jsx'
-import IndexPost from './pages/index.post/Index.post.jsx'
+import PostsPage from './pages/postspage/PostsPage.jsx'
 import DefaultLayout from './layout/DefaultLayout.jsx'
 import ShowPost from './pages/show.post/ShowPost.jsx'
 import './App.css'
-
+import { useState } from 'react'
+import PostsListContext from './contexts/PostsListContext.jsx'
 export default function App() {
-
+  const [postsArray, setPostsArray] = useState([]);
+  function fetchData() {
+    axios
+      .get(`${BASE_URI}/posts/${id}`)
+      .then((res) => {
+        console.log(res.data)
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+        setPost(null);
+      });
+  }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/aboutus' element={<AboutUs />} />
-          <Route path="/blog">
-            <Route index element={<IndexPost />} />
-            <Route path=":id" element={<ShowPost />} />
-          </Route>
-        </Route>
+    <PostsListContext.Provider value={{ postsArray, setPostsArray }}>
 
-      </Routes>
-    </BrowserRouter >
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/aboutus' element={<AboutUs />} />
+            <Route path="/blog">
+              <Route index element={<PostsPage />} />
+              <Route path=":id" element={<ShowPost />} />
+            </Route>
+          </Route>
+
+        </Routes>
+      </BrowserRouter >
+    </PostsListContext.Provider>
   )
 }
---
+++ src/pages/postspage/PostsPage.jsx
@@ -0,0 +1,10 @@
+import IndexPost from "../index.post/Index.post"
+export default function PostsPage() {
+    return (
+        <>
+            <h1>LISTA DEI POST</h1>
+            <IndexPost />
+
+        </>
+    )
+}
